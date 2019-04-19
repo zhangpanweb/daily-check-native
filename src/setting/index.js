a11be7a2f5c6d3aedc72d2184092c2c338a03f46
zhangpanweb <zhangpanweb@163.com>
2019-04-19 15:40:56 +0800

    add delete item

--- src/setting/index.js
+++ src/setting/index.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useReducer } from 'react';
-import { View, Text, Button, FlatList, StyleSheet, TouchableOpacity, AlertIOS, TextInput } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, TouchableOpacity, Alert, AlertIOS, ActionSheetIOS } from 'react-native';
 import fetchRequest from '../../utils/fetch-request';
 
 const Setting = ({ navigation }) => {
   const reducer = (state, action) => {
+    let newState = [];
     switch (action.type) {
       case 'edit':
-        const newState = state.concat([]);
+        newState = state.concat([]);
         newState[action.index] = action.checkItem;
         return newState;
       case 'add':
         return state.concat(action.checkItem);
+      case 'delete':
+        newState = state.concat([]);
+        newState.splice(action.index, 1)
+        return newState;
       default:
         return state;
     }
@@ -23,33 +28,80 @@ const Setting = ({ navigation }) => {
     navigation.setParams({ handleAddItem })
   }, []);
 
-  const handleOpenEditItemModal = (index) => {
-    const item = checkItems[index];
+  const handleAddItem = (newItem) => {
+    dispatchCheckItems({ type: 'add', checkItem: newItem })
+  }
 
+  const showActionSheet = (index) => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['取消', '编辑', '删除'],
+        cancelButtonIndex: 0,
+        destructiveButtonIndex: 2,
+      },
+      (buttionIndex) => {
+        if (buttionIndex === 1) {
+          _handleOpenEditItemModal(index)
+        }
+        if (buttionIndex === 2) {
+          _handleOpenDeleteItemModal(index)
+        }
+      }
+    )
+  }
+
+  const _handleOpenEditItemModal = (index) => {
+    const item = checkItems[index];
     AlertIOS.prompt(
       '编辑打卡项',
       '',
       async (text) => {
-        await _saveCheckItem(item.id, text);
-        const newCheckItem = Object.assign(item, { name: text })
-        dispatchCheckItems({ type: 'edit', index: index, checkItem: newCheckItem })
+        await _saveCheckItem(index, text);
       },
       'plain-text',
       item.name
     )
   };
 
-  const handleAddItem = (newItem) => {
-    dispatchCheckItems({ type: 'add', checkItem: newItem })
+  const _handleOpenDeleteItemModal = (index) => {
+    AlertIOS.alert(
+      '删除打卡项',
+      '确认删除打卡项？',
+      [{
+        text: '取消',
+        onPress: () => { },
+        style: 'cancel'
+      }, {
+        text: '确认',
+        onPress: async (text) => {
+          await _deleteCheckItem(index);
+        },
+      }]
+
+    )
+  };
+
+  const _saveCheckItem = async (index, text) => {
+    const item = checkItems[index];
+    await fetchRequest(`/api/check_item/${item.id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        name: text
+      })
+    });
+    const newCheckItem = Object.assign(item, { name: text })
+    dispatchCheckItems({ type: 'edit', index: index, checkItem: newCheckItem })
   }
 
-  const _saveCheckItem = async (id, name) => {
+  const _deleteCheckItem = async (index) => {
+    const id = checkItems[index].id;
     await fetchRequest(`/api/check_item/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
-        name
+        enabled: 0
       })
-    });
+    })
+    dispatchCheckItems({ type: 'delete', index: index })
   }
 
   const _getCheckItems = async () => {
@@ -63,7 +115,7 @@ const Setting = ({ navigation }) => {
       <FlatList
         data={checkItems}
         renderItem={({ item, index }) => (
-          <TouchableOpacity onPress={() => handleOpenEditItemModal(index)}>
+          <TouchableOpacity onLongPress={() => { showActionSheet(index) }}>
             <Text style={[styles.item]}>{item.name}</Text>
           </TouchableOpacity>
         )}
@@ -72,6 +124,7 @@ const Setting = ({ navigation }) => {
     </View>
   )
 }
+
 
 Setting.navigationOptions = ({ navigation }) => {
   const handleAddItem = navigation.getParam('handleAddItem');
